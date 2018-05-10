66e108e66387f753e8b2300b556918b5983ee077
xmlscript <xmlscript@gmail.com>
2018-05-10 12:21:46 +0800

    dev

--- index.php
+++ index.php
@@ -9,7 +9,6 @@ echo $token, '<br>', $_SERVER['HTTP_REFERER']??'noref', '<hr>';
 //TODO 为了速度，公众号菜单入口只需要静默授权，通过openid保证唯一性
 //TODO 此时将$token->openid存入数据库，作为单独用户出现，除非日后额外绑定手机号是再合并账户
 
-
 //TODO 获取信息这一步骤，似乎没有战略意义，因为昵称头像等信息没有价值
 var_dump(new wx\user($token,'en'));
 
