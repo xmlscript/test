b2edaa113c6fe65b6a139b7d28f228aa044515f0
xmlscript <xmlscript@gmail.com>
2018-05-10 14:28:11 +0800

    dev

--- api.php
+++ api.php
@@ -4,6 +4,7 @@ require 'vendor/autoload.php';
 header('Content-Type: application/json;charset=utf-8');
 die(json_encode(new wx\config(new mp\token($_ENV['APPID'],$_ENV['SECRET']),$url)));
 
+
 die((new class extends srv\api{
   function GET(string $url):wx\config{
     return new wx\config(new mp\token($_ENV['APPID'],$_ENV['SECRET']),$url);
