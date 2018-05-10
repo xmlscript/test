217fb2879afb8ca9e9b999c4c9c90d184a8c2d79
xmlscript <xmlscript@gmail.com>
2018-05-10 16:01:23 +0800

    dev

--- api.php
+++ api.php
@@ -1,12 +1,8 @@
 <?php
 require 'vendor/autoload.php';
 
-//header('Content-Type: application/json;charset=utf-8');
-//die(json_encode(new wx\config(new mp\token($_ENV['APPID'],$_ENV['SECRET']),$_GET['url'])));
-
-
 die((new class extends srv\api{
   function GET(string $url):wx\config{
     return new wx\config(new mp\token($_ENV['APPID'],$_ENV['SECRET']),$url);
   }
-})());
+})());//TODO 还是__toString能省下不少敲击
