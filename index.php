e28d890cfa04fdb9f56ba01508e667e5f994f93a
xmlscript <xmlscript@gmail.com>
2018-05-10 17:49:15 +0800

    dev

--- index.php
+++ index.php
@@ -8,14 +8,16 @@ $(document).ready(()=>{
 
   $.getJSON('/api.php',{url:location.href}, json=>{
 
-      let cfg = Object.assign(
-          json,
-          {jsApiList: [
-          'checkJsApi',
-          'openLocation',
-          'getLocation'
-        ]}
-      )
+    let cfg = Object.assign(
+        json,
+        {jsApiList: [
+        'checkJsApi',
+        'openLocation',
+        'getLocation'
+      ]}
+    )
+
+    alert(JSON.stringify(cfg)) //SPA在此时可以更新签名
 
     wx.config(cfg)
 
