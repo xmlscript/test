4afc486ffd950918930451504353a3d786e62d77
xmlscript <xmlscript@gmail.com>
2018-05-10 12:26:14 +0800

    dev

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ echo '<br>';
 
 $token = wx\token::code($_ENV['APPID'],$_ENV['SECRET'],$_GET['code']);
 
-echo $token, '<br>', $_SERVER['HTTP_REFERER']??'noref', '<hr>';
+echo $token, '<br>', $_SERVER['HTTP_REFERER']??$_SERVER['HTTP_REFERRER']??'no-ref', '<hr>';
 
 //TODO 为了速度，公众号菜单入口只需要静默授权，通过openid保证唯一性
 //TODO 此时将$token->openid存入数据库，作为单独用户出现，除非日后额外绑定手机号是再合并账户
