51554efa103e0c5bf884c5f8da1f71e7afcec617
xmlscript <xmlscript@gmail.com>
2018-05-10 14:17:03 +0800

    dev

--- api.php
+++ api.php
@@ -6,8 +6,6 @@ var_dump($_GET);
 die(json_encode(new wx\config(new mp\token($_ENV['APPID'],$_ENV['SECRET']),$url)));
 
 
-die;
-
 die((new class extends srv\api{
   function GET(string $url):wx\config{
     return new wx\config(new mp\token($_ENV['APPID'],$_ENV['SECRET']),$url);
