03756e95a0e7e64f573a22147dcefc88a322ad52
xmlscript <xmlscript@gmail.com>
2018-05-10 12:04:29 +0800

    dev

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php require 'vendor/autoload.php';
 var_dump($_GET);
+echo '<br>';
 
 $token = wx\token::code($_ENV['APPID'],$_ENV['SECRET'],$_GET['code']);
 
-var_dump("$token");
+echo $token, '<br>', $_SERVER['HTTP_REFERRER']??'noref', '<hr>';
 
+//TODO 为了速度，公众号菜单入口只需要静默授权，通过openid保证唯一性
 
 var_dump(new wx\user($token));
 
