963edc9e02d103469a7e6874a3224fe1320357e2
xmlscript <xmlscript@gmail.com>
2018-05-11 10:15:11 +0800

    dev

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <?=md5(filemtime(__FILE__))?>
+<?=$_ENV['APPID'],$_ENV['SECRET']?>
 
 <script src=https://cdn.jsdelivr.net/npm/jquery@3.3.1/dist/jquery.min.js></script>
 <script src=http://res.wx.qq.com/open/js/jweixin-1.2.0.js></script>
