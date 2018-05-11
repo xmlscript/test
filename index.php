57cc4713568ca00c9bf723176912c291f5429d12
xmlscript <xmlscript@gmail.com>
2018-05-11 16:35:13 +0800

    dev

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <?=md5(filemtime(__FILE__))?>
 <?php
+require 'vendor/autoload.php';
 $token = new mp\token($_ENV['APPID'],$_ENV['SECRET']);
 $ticket = new wx\ticket($token,'jsapi');
 echo '<hr>';
