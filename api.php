aa61c218b3d80bed9a5a38c5c4051f8894620627
xmlscript <xmlscript@gmail.com>
2018-05-12 12:13:48 +0800

    dev

--- api.php
+++ api.php
@@ -1,8 +1,8 @@
 <?php
 require 'vendor/autoload.php';
 
-die((new class extends srv\api{
+die(new class extends srv\api{
   function GET(string $url){
     return new wx\config(new mp\token(getenv('APPID'),getenv('SECRET')),$url);
   }
-})());//TODO 还是__toString能省下不少敲击
+});
