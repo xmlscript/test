935f0361a052a3e239b4978322ae0c7a43acd912
xmlscript <xmlscript@gmail.com>
2018-05-17 18:23:38 +0800

    m

--- callback.php
+++ callback.php
@@ -25,13 +25,9 @@ class msg extends cb\message{
         return $reply->text($token);
 
       case 'ls':
-        try{
-          $media = new mp\media($token);
-          $wc = $media->get_materialcount();
-          return $reply->text(json_encode($wc));
-        }catch(Throwable $e){
-          return $e->getMessage();
-        }
+        $media = new mp\media($token);
+        $wc = $media->get_materialcount();
+        return $reply->text(json_encode($wc));
 
       case 'menu':
         return $reply->text(new mp\menu($token));
