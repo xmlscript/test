603bc8d52f0b3b70d99b3afd56dc7a56f9c6e0a5
xmlscript <xmlscript@gmail.com>
2018-06-03 16:32:02 +0800

    m

--- ip.php
+++ ip.php
@@ -1,14 +1,18 @@
 <?php
 require 'vendor/autoload.php';
 
-echo '<p>HTTP_X_FORWARDED_FOR: <mark>', $_SERVER['HTTP_X_FORWARDED_FOR'], '</mark>';
+echo '<p>HTTP_X_FORWARDED_FOR: <mark>', $_SERVER['HTTP_X_FORWARDED_FOR']??'NULL', '</mark>', PHP_EOL;
 
-echo '<p>REMOTE_ADDR: <mark>', $_SERVER['REMOTE_ADDR'], '</mark>';
-
-
-echo '<p>真实IP: <mark>', new http\ip('10.129.6.1', '10.129.8.1'), '</mark>';
+echo '<p>REMOTE_ADDR: <mark>', $_SERVER['REMOTE_ADDR'], '</mark>', PHP_EOL;
 
 
+echo '<p>真实IP: <mark>', new http\ip('10.129.6.1', '10.129.8.1'), '</mark>', PHP_EOL;
+
+die;
+
+
+echo '<h3>$_SERVER</h3><pre>';
+var_dump($_SERVER);
 
 echo '<h3>ENV</h3><pre>';
 var_dump($_ENV);
