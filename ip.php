416b86728a97a9423671551e74517dbad6015d1d
xmlscript <xmlscript@gmail.com>
2018-06-03 16:45:38 +0800

    m

--- ip.php
+++ ip.php
@@ -8,8 +8,6 @@ echo '<p>REMOTE_ADDR: <mark>', $_SERVER['REMOTE_ADDR'], '</mark>', PHP_EOL;
 
 echo '<p>真实IP: <mark>', $obj=new http\ip('10.129.6.1', '10.129.8.1'), '</mark>', PHP_EOL;
 
-var_dump($obj);
-
 die;
 
 
