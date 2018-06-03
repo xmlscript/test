5e58312baf4b0bd07a670b3791f7ef3b9c4a3d87
xmlscript <xmlscript@gmail.com>
2018-06-03 13:44:07 +0800

    m

--- ip.php
+++ ip.php
@@ -6,8 +6,8 @@ echo '<p>HTTP_X_FORWARDED_FOR: ', $_SERVER['HTTP_X_FORWARDED_FOR'];
 echo '<p>REMOTE_ADDR: ', $_SERVER['REMOTE_ADDR'];
 
 
-echo '<h3>真实IP</h3><p>', new http\ip('10.129.8.1');
+echo '<h3>真实IP</h3><mark>', new http\ip('10.129.6.1'), '</mark>';
 
 
-echo '<hr><pre>';
-var_dump($_SERVER);
+echo '<h3>ENV</h3><pre>';
+var_dump($_ENV);
